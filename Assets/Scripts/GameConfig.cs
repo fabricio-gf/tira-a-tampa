3875b796fc78d22d4f1f0f8724783a28cd39d020
Fabricio <fabricio-gf@hotmail.com>
2018-11-24 17:11:33 -0200

    Added animation on robot and config file

---
+++ Assets/Scripts/GameConfig.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Config", menuName = "Config")]
+public class GameConfig : ScriptableObject {
+
+	[Range(1,10)] public int speed;
+	[Range(20,50)] public int populations;
+	[Range(1,3)] public int difficulty;
+}
